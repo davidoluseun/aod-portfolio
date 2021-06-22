e393be11d9dbc38bfd969e0d21092c8af502fd2c
David Oluseun <a.davidoluseun@gmail.com>
2021-06-22 12:29:44 +0100

    Hide and show ScrollToTop
    Hide ScrollToTop but makes it visible when the page is scrolled vertically above half height.

--- src/components/common/ScrollToTop.tsx
+++ src/components/common/ScrollToTop.tsx
@@ -4,7 +4,25 @@ import { animateScroll as scroll } from "react-scroll";
 import { FaRocket } from "react-icons/fa";
 
 const ScrollToTop = () => {
+  const [isVisible, setIsVisible] = React.useState(false);
   const { colorMode } = useColorMode();
+
+  const revealToTop = React.useCallback(() => {
+    let halfHeight = document.body.clientHeight / 2;
+
+    if (window.scrollY > halfHeight && isVisible === false) setIsVisible(true);
+
+    if (window.scrollY < halfHeight && isVisible === true) setIsVisible(false);
+  }, [isVisible]);
+
+  React.useEffect(() => {
+    window.addEventListener("scroll", revealToTop);
+
+    return () => {
+      window.removeEventListener("scroll", revealToTop);
+    };
+  }, [isVisible, revealToTop]);
+
   const handleToTop = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
 
@@ -15,7 +33,7 @@ const ScrollToTop = () => {
   };
 
   return (
-    <Box>
+    <Box display={isVisible ? "block" : "none"}>
       <IconButton
         bg="transparent !important"
         pt="2"
